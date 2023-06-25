2c33bed860e848d82ee6a50365d674c0336ad2dd
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2023-06-25 15:01:48 -0300

    setup lading page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LandingPage from './pages/landing-page';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import LandingPage from "./pages/landing-page";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <LandingPage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
